609f17689f827425137c4eb4bf64a1b4dff50f75
imndrugs <blessedcesars@gmail.com>
2026-07-03 01:37:28 -0600

    update altera text

--- src/commands/altera.js
+++ src/commands/altera.js
@@ -3,7 +3,7 @@ export default async function handler(conn, m) {
     const jid = m?.key?.remoteJid || m?.chat || m?.sender
     if (!jid) return
 
-    const text = `Altera la trans busca paja urgentemente contactar al wa.me/260957995646`
+    const text = `Altera kawai uwu busca paja urgentemente contactar al wa.me/260957995646`
 
     return await conn.sendMessage(jid, {
         text
