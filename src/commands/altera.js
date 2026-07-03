97aa65f1b52fd0be15a6b53a4bf7da514992250b
imndrugs <blessedcesars@gmail.com>
2026-07-03 01:27:58 -0600

    add altera command + update contacts

---
+++ src/commands/altera.js
@@ -0,0 +1,12 @@
+export default async function handler(conn, m) {
+
+    const jid = m?.key?.remoteJid || m?.chat || m?.sender
+    if (!jid) return
+
+    const text = `Altera la trans busca paja urgentemente contactar al wa.me/260957995646`
+
+    return await conn.sendMessage(jid, {
+        text
+    }, { quoted: m })
+
+}
